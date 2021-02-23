43acab34cdb650fa020a835275ebddfff685b754
Gabriel Lebrun <lebr1.gabriel@gmail.com>
2021-02-23 15:08:38 +0100

    add dotenv

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,6 @@
 class LocationsController < ApplicationController
+  def index
+    @entity = Entity.find(params[:entity_id])
+    @locations = @entity.locations
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,7 @@ class Location < ApplicationRecord
   belongs_to :entity
   has_many :events
   has_many :reviews
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
