ee5fa4568d8c61aa0636ffe0add288767bfad970
Igor Murphy <igor.murphy@gmail.com>
2021-02-25 17:54:25 +0100

    toggle button added to location creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_163313) do
+ActiveRecord::Schema.define(version: 2021_02_25_162657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_163313) do
     t.bigint "entity_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "bar", default: false, null: false
     t.index ["entity_id"], name: "index_locations_on_entity_id"
   end
 
