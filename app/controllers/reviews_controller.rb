a1e6290fed490b9549fd28db2a8a1bb835fa2302
Igor Murphy <igor.murphy@gmail.com>
2021-02-25 15:10:22 +0100

    green button, a version of new location and early new review...

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @location = Location.find(params[:location_id])
+    # raise
+  end
+  def create
+    @review = Review.new(review_params)
+    @location = Location.find(params[:location_id])
+    @review.user_id = current_user
+    if @review.save
+      redirect_to location_path(@location)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
