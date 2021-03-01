ede37e6eea2a6719e19e818daed60a6a1340af47
Igor Murphy <igor.murphy@gmail.com>
2021-03-01 17:38:16 +0100

    added default images for new location or if no uploaded image

--- app/javascript/functions/show_review_div.js
+++ app/javascript/functions/show_review_div.js
@@ -1,12 +1,21 @@
 const showReviewDiv = () => {
   const btn = document.getElementById('location-review');
   const newReviewDiv = document.getElementById('new-review');
+  const show = document.querySelector('.show')
+
   if (btn && newReviewDiv) {
     btn.addEventListener('click', (event) => {
       event.preventDefault();
       newReviewDiv.classList.remove('location-hide');
     })
   }
+
+  if (show) {
+    show.addEventListener('click', () => {
+      newReviewDiv.classList.add('location-hide');
+    })
+  }
+
 }
 
 export { showReviewDiv };
