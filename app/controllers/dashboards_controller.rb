2d657c53293c3795bb7b5793dcc5db8b53380eba
Gabriel Lebrun <lebr1.gabriel@gmail.com>
2021-03-03 11:56:57 +0100

    changes added

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class DashboardsController < ApplicationController
   def dashboard
-    @pending_events = current_user.pending_events_as_attendee
-    @events = current_user.accepted_events_as_attendee + current_user.events.distinct
+    @pending_events = current_user.pending_events_as_attendee.upcoming
+    # @pending_events = @pending_events.where('start_at >= ?', Date.today)
+    @events = current_user.accepted_events_as_attendee.upcoming + current_user.events.upcoming.distinct
   end
 end
