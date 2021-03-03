2f9024a73344e7e04bcf38ac6525a08b6f0ab0ec
keziah smadja <keziah.smadja@gmail.com>
2021-03-03 17:04:54 +0100

    seed changed a bit but not verified

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,13 @@ puts 'Finished'
 
 # creation of data in the tables starting with those with no dependencies
 puts 'Creating entities...'
-apple = Entity.new(
-  address: "12 Rue Halévy, 75009 Paris",
-  name: "Apple"
+jojobande = Entity.new(
+  address: "Unit 22 Tooley St, London SE1 2HD, Royaume-Uni",
+  name: "Joseph's crew"
   )
-file = URI.open('https://static.lexpress.fr/medias_9606/w_1600,h_696,c_crop,x_0,y_285/w_480,h_270,c_fill,g_north/v1402566386/apple-logo-pomme_4918775.jpeg')
-apple.photo.attach(io: file, filename: 'apple.jpg', content_type: 'image/jpg')
-apple.save!
+file = URI.open('http://res.cloudinary.com/dz1ndp7wi/image/upload/v1614786328/lv9pxubcuzbnfvaeo68x.jpg')
+jojobande.photo.attach(io: file, filename: 'jojo.jpg', content_type: 'image/jpg')
+jojobande.save!
 
 
 
@@ -161,14 +161,14 @@ UserEntity.create!(
 
 UserEntity.create!(
   user_id: demoman.id,
-  entity_id: apple.id,
+  entity_id: jojobande.id,
   role: "Demo guy",
   status: 0
   )
 
 UserEntity.create!(
   user_id: jo.id,
-  entity_id: apple.id,
+  entity_id: jojobande.id,
   role: "student",
   status: 0
   )
@@ -176,28 +176,28 @@ UserEntity.create!(
 
 UserEntity.create!(
   user_id: gab.id,
-  entity_id: apple.id,
+  entity_id: jojobande.id,
   role: "student",
   status: 0
   )
 
 UserEntity.create!(
   user_id: kez.id,
-  entity_id: apple.id,
+  entity_id: jojobande.id,
   role: "student",
   status: 0
   )
 
 UserEntity.create!(
   user_id: tom.id,
-  entity_id: apple.id,
+  entity_id: jojobande.id,
   role: "teacher",
   status: 0
   )
 
 UserEntity.create!(
   user_id: igo.id,
-  entity_id: apple.id,
+  entity_id: jojobande.id,
   role: "student",
   status: 0
   )
@@ -256,98 +256,6 @@ tchai = Location.new(
   )
 tchai.photo.attach(io: file, filename: 'tachai.jpg', content_type: 'image/jpg')
 tchai.save!
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352331/zmvcm4iiydjbxgcxyrqf.jpg')
-alicheur = Location.new(
-  address: "96 Rue Saint-Maur, 75011 Paris",
-  name: "Alicheur",
-  category: "Health",
-  entity_id: wagon.id
-  )
-alicheur.photo.attach(io: file, filename: 'alicheur.jpg', content_type: 'image/jpg')
-alicheur.save!
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614615478/ll7op7jbnw25wf0xqkah.jpg')
-thai = Location.new(
-  address: "121 rue Oberkampf, 75011 Paris",
-  name: "Thai Wok House",
-  category: "Asian",
-  entity_id: wagon.id
-  )
-thai.photo.attach(io: file, filename: 'thai.jpg', content_type: 'image/jpg')
-thai.save!
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614351804/qy62ky380bbeptoijunq.jpg')
-jbun = Location.new(
-  address: "126 Rue Oberkampf, 75011 Paris",
-  name: "James Bún",
-  category: "Asian",
-  entity_id: wagon.id
-  )
-jbun.photo.attach(io: file, filename: 'jbun.jpg', content_type: 'image/jpg')
-jbun.save!
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614351796/rogwedj8ly0unzlbzibg.jpg')
-pokawa = Location.new(
-  address: "56 bis Rue Oberkampf, 75011 Paris",
-  name: "Pokawa",
-  category: "Hawaiian",
-  entity_id: wagon.id
-  )
-pokawa.photo.attach(io: file, filename: 'pokawa.jpg', content_type: 'image/jpg')
-pokawa.save!
-
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352269/w04cm0gy2yhc31k901o9.jpg')
-justine = Location.new(
-  address: "96 Rue Oberkampf, 75011 Paris",
-  name: "Chez Justine",
-  category: "Pizza",
-  entity_id: wagon.id
-  )
-justine.photo.attach(io: file, filename: 'justine.jpg', content_type: 'image/jpg')
-justine.save!
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352193/og7ydrcrws0mfptwh4v2.jpg')
-
-pny = Location.new(
-  address: "96 Rue Oberkampf, 75011 Paris",
-  name: "PNY",
-  category: "Burger",
-  entity_id: wagon.id
-  )
-pny.photo.attach(io: file, filename: 'pny.jpg', content_type: 'image/jpg')
-pny.save!
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352176/dlqu1xk5slubj05obn6t.jpg')
-camion = Location.new(
-  address: "66 Rue Oberkampf, 75011 Paris",
-  name: "Le Camion Qui Fume",
-  category: "Burger",
-  entity_id: wagon.id
-  )
-camion.photo.attach(io: file, filename: 'camion.jpg', content_type: 'image/jpg')
-camion.save!
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352120/f0mdibdbadqavxuvnh1e.jpg')
-nakagawa = Location.new(
-  address: "142 Rue Oberkampf, 75011 Paris",
-  name: "Nakagawa",
-  category: "Sushi",
-  entity_id: wagon.id
-  )
-nakagawa.photo.attach(io: file, filename: 'nakagawa.jpg', content_type: 'image/jpg')
-nakagawa.save!
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352104/mxyeri6tcp7n3i1j2jjz.jpg')
-brigade = Location.new(
-  address: "103 Rue Oberkampf, 75011 Paris",
-  name: "La Brigade",
-  category: "Bistrot",
-  entity_id: wagon.id
-  )
-brigade.photo.attach(io: file, filename: 'brigade.jpg', content_type: 'image/jpg')
-brigade.save!
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352093/p42phpta6mddrndpmikq.jpg')
-phenicien = Location.new(
-  address: "101 Rue Oberkampf, 75011 Paris",
-  name: "Le Petit Phénicien",
-  category: "Lebanese",
-  entity_id: wagon.id
-  )
-phenicien.photo.attach(io: file, filename: 'phenicien.jpg', content_type: 'image/jpg')
-phenicien.save!
 file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352081/bwsisjppbeduvughtuiv.jpg')
 bocamexa = Location.new(
   address: "95 Rue Oberkampf, 75011 Paris",
@@ -374,76 +282,39 @@ psang = Location.new(
 psang.photo.attach(io: file, filename: 'psang.jpg', content_type: 'image/jpg')
 psang.save!
 
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352193/og7ydrcrws0mfptwh4v2.jpg')
-hippopotamus = Location.new(
-  address: "1 Boulevard des Capucines, 75002 Paris",
-  name: "Hippopotamus",
-  category: "Burger",
-  entity_id: apple.id
-  )
-hippopotamus.photo.attach(io: file, filename: 'hippopotamus.jpg', content_type: 'image/jpg')
-hippopotamus.save!
-
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352193/og7ydrcrws0mfptwh4v2.jpg')
-hanoicaphe = Location.new(
-  address: "30 Boulevard des Italiens, 75009 Paris",
-  name: "Hanoï Cà Phê",
-  category: "Asian",
-  entity_id: apple.id
-  )
-hanoicaphe.photo.attach(io: file, filename: 'hanoicaphe.jpg', content_type: 'image/jpg')
-hanoicaphe.save!
-
-operasandwich = Location.new(
-  address: "1 Rue de Marivaux, 75002 Paris",
-  name: "Opera Sandwich",
-  category: "Sandwich",
-  entity_id: apple.id
-  )
-operasandwich.save!
-
-file = URI.open('https://res.cloudinary.com/dz1ndp7wi/image/upload/v1614352193/og7ydrcrws0mfptwh4v2.jpg')
-cafeopera = Location.new(
-  address: "9 Rue de Mogador, 75009 Paris",
-  name: "Café de l'Opéra",
-  category: "Bistrot",
-  entity_id: apple.id
-  )
-cafeopera.photo.attach(io: file, filename: 'cafeopera.jpg', content_type: 'image/jpg')
-cafeopera.save!
 
 puts 'Finished!'
 
 puts 'Creating events...'
 lunch_two = Event.new(
   location_id: favori.id,
-  user_id: jo.id,
-  start_at: Time.strptime('02/27/2021 12:45', '%m/%d/%Y %H:%M'),
+  user_id: kez.id,
+  start_at: Time.strptime('03/08/2021 20:30', '%m/%d/%Y %H:%M'),
   public: false,
   capacity: 2
   )
 lunch_two.save!
 lunch_three = Event.new(
-  location_id: favori.id,
-  user_id: jo.id,
-  start_at: Time.now,
+  location_id: barracao.id,
+  user_id: gab.id,
+  start_at: Time.strptime('03/08/2021 22:30', '%m/%d/%Y %H:%M'),
   public: false,
   capacity: 2
   )
 lunch_three.save!
 lunch_public = Event.new(
-  location_id: surpriz.id,
-  user_id: kez.id,
-  start_at: Time.now,
+  location_id: barracao.id,
+  user_id: demo.id,
+  start_at: Time.strptime('03/05/2021 20:30', '%m/%d/%Y %H:%M'),
   public: true,
-  capacity: rand(2..12)
+  capacity: rand(2..6)
   )
 lunch_public.save!
 puts 'Finished!'
 
 puts 'Creating attendees...'
 lunch_two_attendee_one = Attendee.new(
-  user_id: demoman.id,
+  user_id: jo.id,
   event_id: lunch_two.id,
   status: 0
   )
@@ -459,15 +330,9 @@ lunch_three_attendee_one = Attendee.new(
   event_id: lunch_three.id,
   status: 0
   )
-lunch_three_attendee_one.save!
-lunch_public_attendee_one = Attendee.new(
-  user_id: kez.id,
-  event_id: lunch_public.id,
-  status: 1
-  )
 lunch_public_attendee_one.save!
 lunch_public_attendee_two = Attendee.new(
-  user_id: jo.id,
+  user_id: kez.id,
   event_id: lunch_public.id,
   status: 0
   )
@@ -508,13 +373,6 @@ Review.create!(
   comment: "Good value for money.",
   rating: 4,
   user_id: kez.id
-  )
-
-Review.create!(
-  location_id: thai.id,
-  comment: "Really good Loc Lac.",
-  rating: 4,
-  user_id: igo.id
   )
 
 puts 'Finished!'
