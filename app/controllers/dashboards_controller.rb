1eeac49f27ba95b430e81a8f8110de2924b47e37
Joseph Dulac <dulacjoseph@gmail.com>
2021-03-04 14:38:52 +0100

    donedonedone

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,11 @@
 class DashboardsController < ApplicationController
   def dashboard
-    @pending_events = current_user.pending_events_as_attendee.upcoming
+    @entity = Entity.find(params[:id])
+    @events_as_attendee = @entity.events.joins(:attendees).where(attendees: {user_id:current_user.id })
+    @pending_events = @events_as_attendee.upcoming.where(attendees: {status: 'pending' })
+    @accepted_events = @events_as_attendee.upcoming.where(attendees: {status: 'accepted' })
+    # @pending_events = current_user.pending_events_as_attendee.upcoming
     # @pending_events = @pending_events.where('start_at >= ?', Date.today)
-    @events = current_user.accepted_events_as_attendee.upcoming + current_user.events.upcoming.distinct
+    @events = @accepted_events + current_user.events.upcoming.distinct
   end
 end
