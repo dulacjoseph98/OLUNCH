db2c6711ff689da72e2c60c3b027d0be1ae728bc
Igor Murphy <igor.murphy@gmail.com>
2021-03-04 10:21:39 +0100

    temp save

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -2,9 +2,12 @@ import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
   flatpickr(".datepicker", {
-    noCalendar: true,
+    // noCalendar: true,
+    minDate: "today",
+    maxDate: new Date().fp_incr(14), // 14 days from now works well but not when you add enableTime...
     enableTime: true,
-    time_24hr: true
+    // time_24hr: true,
+    defaultMinute: 0
   });
 }
 
