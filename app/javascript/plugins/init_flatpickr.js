6d316fae9da724d1b0ef205bd14bef9fb7fffdbd
Igor Murphy <igor.murphy@gmail.com>
2021-03-04 11:31:54 +0100

    fix attendee

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -3,12 +3,13 @@ import flatpickr from "flatpickr";
 const initFlatpickr = () => {
   flatpickr(".datepicker", {
     // noCalendar: true,
-    minDate: "today",
+    minDate: 'today',
     maxDate: new Date().fp_incr(14), // 14 days from now works well but not when you add enableTime...
     enableTime: true,
     // time_24hr: true,
-    defaultMinute: 0
-    // minuteIncrement: 15,
+    // defaultMinute: 0,
+    minuteIncrement: 15,
+    // disableMobile: true
   });
 }
 
